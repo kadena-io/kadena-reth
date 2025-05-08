f79a81ae79e24cca5bbe6df4f3189531e6c2f7dc
Lars Kuhtz <lars@kadena.io>
2025-05-07 17:32:20 -0700

    remove outdated custom enging args parser

--- src/main.rs
+++ src/main.rs
@@ -3,107 +3,40 @@
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
 
-use clap::{Args, Parser};
+use clap::Parser;
 use custom_evm::KadenaExecutorBuilder;
 use reth::cli::Cli;
 use reth_ethereum_cli::chainspec::EthereumChainSpecParser;
-use reth_node_api::{TreeConfig, DEFAULT_MEMORY_BLOCK_BUFFER_TARGET};
-use reth_node_builder::EngineNodeLauncher;
-use reth_node_core::node_config::DEFAULT_PERSISTENCE_THRESHOLD;
+use reth_node_builder::NodeHandle;
 use reth_node_ethereum::{node::EthereumAddOns, EthereumNode};
 use reth_provider::providers::BlockchainProvider;
-use reth_tracing::tracing::warn;
+use tracing::info;
 
 pub mod custom_evm;
 pub mod kadena_precompiles;
 
-/// Parameters for configuring the engine
-#[derive(Debug, Clone, Args, PartialEq, Eq)]
-#[command(next_help_heading = "Engine")]
-pub struct EngineArgs {
-    /// Enable the experimental engine features on reth binary
-    ///
-    /// DEPRECATED: experimental engine is default now, use --engine.legacy to enable the legacy
-    /// functionality
-    #[arg(long = "engine.experimental", default_value = "false")]
-    pub experimental: bool,
-
-    /// Enable the legacy engine on reth binary
-    #[arg(long = "engine.legacy", default_value = "false")]
-    pub legacy: bool,
-
-    /// Configure persistence threshold for engine experimental.
-    #[arg(long = "engine.persistence-threshold", requires = "experimental", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
-    pub persistence_threshold: u64,
-
-    /// Configure the target number of blocks to keep in memory.
-    #[arg(long = "engine.memory-block-buffer-target", requires = "experimental", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
-    pub memory_block_buffer_target: u64,
-}
-
-impl Default for EngineArgs {
-    fn default() -> Self {
-        Self {
-            experimental: false,
-            legacy: false,
-            persistence_threshold: DEFAULT_PERSISTENCE_THRESHOLD,
-            memory_block_buffer_target: DEFAULT_MEMORY_BLOCK_BUFFER_TARGET,
-        }
-    }
-}
-
 fn main() {
+    reth_cli_util::sigsegv_handler::install();
+
     // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
     if std::env::var_os("RUST_BACKTRACE").is_none() {
-        std::env::set_var("RUST_BACKTRACE", "1");
+        unsafe { std::env::set_var("RUST_BACKTRACE", "1") };
     }
 
     if let Err(err) =
-        Cli::<EthereumChainSpecParser, EngineArgs>::parse().run(|builder, engine_args| async move {
-            if engine_args.experimental {
-                warn!(target: "reth::cli", "Experimental engine is default now, and the --engine.experimental flag is deprecated. To enable the legacy functionality, use --engine.legacy.");
-            }
-
-            let launcher = EngineNodeLauncher::new(
-                builder.task_executor().clone(),
-                builder.config().datadir(),
-                TreeConfig::default()
-                    .with_persistence_threshold(engine_args.persistence_threshold)
-                    .with_memory_block_buffer_target(engine_args.memory_block_buffer_target),
-            );
-
-
-            let handle = builder
-              .with_types_and_provider::<EthereumNode, BlockchainProvider<_>>()
-              .with_components(EthereumNode::components().executor(KadenaExecutorBuilder::default()))
-              .with_add_ons(EthereumAddOns::default())
-              .launch_with(launcher)
-              .await.unwrap();
-
-            handle.node_exit_future.await
+        Cli::<EthereumChainSpecParser>::parse().run(|builder, _| async move {
+            info!(target: "reth::cli", "Launching node");
+            let NodeHandle { node_exit_future, node: _ } = builder
+                .with_types_and_provider::<EthereumNode, BlockchainProvider<_>>()
+                .with_components(EthereumNode::components().executor(KadenaExecutorBuilder::default()))
+                .with_add_ons(EthereumAddOns::default())
+                .launch_with_debug_capabilities()
+                .await?;
+            node_exit_future.await
         })
-        {
-            eprintln!("Error: {err:?}");
-            std::process::exit(1);
-        }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use clap::Parser;
-
-    /// A helper type to parse Args more easily
-    #[derive(Parser)]
-    struct CommandParser<T: Args> {
-        #[command(flatten)]
-        args: T,
-    }
-
-    #[test]
-    fn test_parse_engine_args() {
-        let default_args = EngineArgs::default();
-        let args = CommandParser::<EngineArgs>::parse_from(["reth"]).args;
-        assert_eq!(args, default_args);
+    {
+        eprintln!("Error: {err:?}");
+        std::process::exit(1);
     }
 }
+
